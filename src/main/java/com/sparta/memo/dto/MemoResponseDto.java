b416af78a3a8808ed112038b62cb4509c1a1cc22
dyorcat <yool0417@gmail.com>
2024-07-21 14:34:00 +0900

    MemoResponseDto 생성자 작성

--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -1,5 +1,6 @@
 package com.sparta.memo.dto;
 
+import com.sparta.memo.entity.Memo;
 import lombok.Getter;
 
 @Getter
@@ -7,4 +8,10 @@ public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.username = memo.getUsername();
+        this.contents = memo.getContents();
+    }
 }
