b02d146adab50e6f12c3ccc921f33ad7588835f8
dyorcat <yool0417@gmail.com>
2024-07-21 14:34:15 +0900

    Memo 생성자 작성

--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -1,6 +1,7 @@
 package com.sparta.memo.entity;
 
 
+import com.sparta.memo.dto.MemoRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,4 +13,9 @@ public class Memo {
     private Long id;
     private String username;
     private String contents;
+
+    public Memo(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
 }
