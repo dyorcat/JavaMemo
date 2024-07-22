0edfa0571bdb87cc743af8f2028fdc2b7aa1216b
dyorcat <yool0417@gmail.com>
2024-07-22 12:21:35 +0900

    어노테이션 추가

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,12 +4,16 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class MemoService {
     private final MemoRepository memoRepository;
 
+    @Autowired // 생략 가능
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
