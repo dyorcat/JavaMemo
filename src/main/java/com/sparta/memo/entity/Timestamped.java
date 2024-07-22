f75b79fdd34f04842669feec58041f031da8736b
dyorcat <yool0417@gmail.com>
2024-07-22 13:58:05 +0900

    JPA Auditing 적용하기

---
+++ src/main/java/com/sparta/memo/entity/Timestamped.java
@@ -0,0 +1,25 @@
+package com.sparta.memo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped { // 이것 자체를 엔티티로 생성할 일은 없기 때문에 abstract 붙임
+
+    @CreatedDate
+    @Column(updatable = false) // 최초 생성 후 수정되면 안 되기 때문.
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
